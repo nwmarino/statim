01f083e72ab5f2bc0069ed9f8b77bea1263c66e3
nwmarino <nwmarinoto@gmail.com>
2025-08-26 15:35:53 -0400

    add siir codegen defs

--- source/tree/visitor.hpp
+++ source/tree/visitor.hpp
@@ -1,11 +1,20 @@
 #ifndef STATIM_VISITOR_HPP_
 #define STATIM_VISITOR_HPP_
 
+#include "siir/basicblock.hpp"
+#include "siir/cfg.hpp"
+#include "siir/function.hpp"
+#include "siir/instbuilder.hpp"
+#include "siir/value.hpp"
 #include "tree/scope.hpp"
 #include "types/options.hpp"
 #include "types/types.hpp"
 
+#include <unordered_map>
+
 namespace stm {
+
+class Type;
 
 class TranslationUnit;
 class Frame;
@@ -49,6 +58,7 @@ class MemberExpr;
 class CallExpr;
 class RuneExpr;
 
+/// Abstract visitor pattern over a syntax tree.
 class Visitor {
 public:
     virtual ~Visitor() = default;
@@ -90,10 +100,13 @@ public:
     virtual void visit(RuneExpr& node) = 0;
 };
 
+/// A light-weight resolution pass over a syntax tree to resolve deferred 
+/// symbol references such as function calls or those referencing imported 
+/// names.
 class SymbolAnalysis final : public Visitor {
-    Options&        opts;
-    Root&           root;
-    const Scope*    pScope;
+    Options& opts;
+    Root& root;
+    const Scope* pScope;
 
 public:
     SymbolAnalysis(Options& opts, Root& root);
@@ -135,19 +148,97 @@ public:
     void visit(RuneExpr& node) override;
 };
 
+/// A semantic pass over a syntax tree to perform language-based validation
+/// like type-checking, implicit casting, loop semantics, etc.
 class SemanticAnalysis final : public Visitor {
-    enum class Loop : u8 {
-        None,
-        While,
-    };
-
-    Options&        opts;
-    Root&           root;
-    FunctionDecl*   pFunction = nullptr;
-    Loop            loop = Loop::None;
+    enum class Loop : u8 { None, While } loop = Loop::None;
+    Options& opts;
+    Root& root;
+    FunctionDecl* pFunction = nullptr;
 
 public:
     SemanticAnalysis(Options& opts, Root& root) : opts(opts), root(root) {};
+
+    void visit(Root& node) override;
+
+    void visit(FunctionDecl& node) override;
+    void visit(ParameterDecl& node) override {};
+    void visit(VariableDecl& node) override;
+    void visit(FieldDecl& node) override {};
+    void visit(StructDecl& node) override {};
+    void visit(EnumValueDecl& node) override {};
+    void visit(EnumDecl& node) override {};
+
+    void visit(BlockStmt& node) override;
+    void visit(BreakStmt& node) override;
+    void visit(ContinueStmt& node) override;
+    void visit(DeclStmt& node) override;
+    void visit(IfStmt& node) override;
+    void visit(WhileStmt& node) override;
+    void visit(RetStmt& node) override;
+    void visit(Rune& node) override;
+
+    void visit(BoolLiteral& node) override {};
+    void visit(IntegerLiteral& node) override {};
+    void visit(FloatLiteral& node) override {};
+    void visit(CharLiteral& node) override {};
+    void visit(StringLiteral& node) override {};
+    void visit(NullLiteral& node) override {};
+    void visit(BinaryExpr& node) override;
+    void visit(UnaryExpr& node) override;
+    void visit(CastExpr& node) override;
+    void visit(ParenExpr& node) override;
+    void visit(SizeofExpr& node) override {};
+    void visit(SubscriptExpr& node) override;
+    void visit(ReferenceExpr& node) override {};
+    void visit(MemberExpr& node) override;
+    void visit(CallExpr& node) override;
+    void visit(RuneExpr& node) override;
+};
+
+/// A code generation pass over a syntax tree root to fill out the complex of
+/// a SIIR control flow graph.
+class Codegen final : public Visitor {
+    enum ValueContext : u8 { LValue, RValue } m_vctx;
+    enum Phase : u8 { Declare, Define } m_phase; 
+    Options& m_opts;
+    Root& m_root;
+    siir::CFG& m_cfg;
+    siir::InstBuilder m_builder;
+    siir::Function* m_func = nullptr;
+    siir::Value* m_tmp = nullptr;
+    siir::Value* m_place = nullptr;
+    siir::BasicBlock* m_cond = nullptr;
+    siir::BasicBlock* m_merge = nullptr;
+    
+    /// Stores cached results of the `mangle` function.
+    std::unordered_map<Decl*, std::string> m_mangled;
+
+    /// Mangle the name of \p decl as per its runes, and cache it for later
+    /// references.
+    const std::string& mangle(Decl* decl);
+
+    /// Lower \p type to its SIIR equivelant, if it can be.
+    const siir::Type* lower_type(const Type* type);
+
+    /// Inject a boolean comparison onto \p value if it isn't already typed
+    /// with `i1`. Used for implicit language comparisons, like `if x { ... }`.
+    siir::Value* inject_bool_cmp(siir::Value* value);
+
+    /// Lower the function \p decl to a new, empty SIIR function with
+    /// arguments. 
+    void lower_function(FunctionDecl* decl);
+    
+    /// Implement the body of function \p decl.
+    void impl_function(FunctionDecl* decl);
+    
+    /// Lower the structure \p decl to a new SIIR StructType. 
+    void lower_structure(StructDecl* decl);
+
+public:
+    Codegen(Options& opts, Root& root, siir::CFG& cfg);
+
+    void run();
 
     void visit(Root& node) override;
 
